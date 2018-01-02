6403a17799d71a5dd78fe194cee25f9e6a8993f8
MarsChang <mars09250148@yahoo.com.tw>
2018-01-02 14:04:23 +0800

    Initial commit

---
+++ app/src/main/java/com/mars/android2018010202/MainActivity.java
@@ -0,0 +1,30 @@
+package com.mars.android2018010202;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox cb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cb = (CheckBox)findViewById(R.id.checkBox);
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                {
+                    Toast.makeText(MainActivity.this, "打勾了", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "取消了", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
